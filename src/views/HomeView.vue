a13eb4bb05ead42ab6a1b54f56fce2f4b973ab24
Brix-D <Shlyakhta4@gmail.com>
2023-06-20 21:03:20 +0400

    feat(list): added large lagging list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,29 @@
 </script>
 
 <template>
-  <main>
+  <main :class="$style['main']">
+    <ul :class="$style['list']">
+      <li :class="$style['list-item']" v-for="i in 100000" :key="i">
+        list item #{{ i }}
+      </li>
+    </ul>
   </main>
 </template>
+
+<style module>
+.main {
+  padding: 20px;
+}
+.list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.list-item {
+  height: 56px;
+  background-color: #fff;
+  border: 1px solid #08ad44;
+  border-radius: 8px;
+  padding: 16px;
+}
+</style>
